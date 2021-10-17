2efb19ef5962cb5df802a16d0c4d5a606af66872
Gabriel Arancio <gabrielarancio@gmail.com>
2021-10-17 17:26:18 -0300

    clases gabi

---
+++ PPAI/PPAI/Entidades/DetalleExposicion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI.Entidades
+{
+    public class DetalleExposicion
+    {
+        public string LugarAsignado { get; set; }
+        public Obra obra { get; set; }
+
+
+    }
+}
---
+++ PPAI/PPAI/Entidades/TipoVisita.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI.Entidades
+{
+    class TipoVisita
+    {
+        public string Nombre { get; set; }
+    }
+}
